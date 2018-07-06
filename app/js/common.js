cb37dbfb2dd1b23ec86f4bd15f85287fcedc24a8
fishouk <fishouk@yandex.ru>
2018-07-06 17:19:58 +0300

    more styles

--- app/js/common.js
+++ app/js/common.js
@@ -160,68 +160,68 @@ $(document).ready(function(){
     //=====  / To top scroll  ======//
 
     //--====   Hid Menu scroll  ====--//
-    var header = $(".header"); // Меню
-    var scrollPrev = 0;// Предыдущее значение скролла
-
-    $(window).scroll(function() {
-        var scrolled = $(window).scrollTop(); // Высота скролла в px
-        var firstScrollUp = false; // Параметр начала сколла вверх
-        var firstScrollDown = false; // Параметр начала сколла вниз
-
-        // Если скроллим
-        if ( scrolled > 0 ) {
-            // Если текущее значение скролла > предыдущего, т.е. скроллим вниз
-            if ( scrolled > scrollPrev ) {
-                firstScrollUp = false; // Обнуляем параметр начала скролла вверх
-                // Если меню видно
-                if ( scrolled < header.height() + header.offset().top ) {
-                    // Если только начали скроллить вниз
-                    if ( firstScrollDown === false ) {
-                        var topPosition = header.offset().top; // Фиксируем текущую позицию меню
-                        header.css({
-                            "top": topPosition + "px"
-                        });
-                        firstScrollDown = true;
-                    }
-                    // Позиционируем меню абсолютно
-                    header.css({
-                        "position": "absolute"
-                    });
-                    // Если меню НЕ видно
-                } else {
-                    // Позиционируем меню фиксированно вне экрана
-                    header.css({
-                        "position": "fixed",
-                        "top": "-" + header.height() + "px"
-                    });
-                }
-
-                // Если текущее значение скролла < предыдущего, т.е. скроллим вверх
-            } else {
-                firstScrollDown = false; // Обнуляем параметр начала скролла вниз
-                // Если меню не видно
-                if ( scrolled > header.offset().top ) {
-                    // Если только начали скроллить вверх
-                    if ( firstScrollUp === false ) {
-                        var topPosition = header.offset().top; // Фиксируем текущую позицию меню
-                        header.css({
-                            "top": topPosition + "px"
-                        });
-                        firstScrollUp = true;
-                    }
-                    // Позиционируем меню абсолютно
-                    header.css({
-                        "position": "absolute"
-                    });
-                } else {
-                    // Убираем все стили
-                    header.removeAttr("style");
-                }
-            }
-            // Присваеваем текущее значение скролла предыдущему
-            scrollPrev = scrolled;
-        }
-    });
+    // var header = $(".header"); // Меню
+    // var scrollPrev = 0;// Предыдущее значение скролла
+    //
+    // $(window).scroll(function() {
+    //     var scrolled = $(window).scrollTop(); // Высота скролла в px
+    //     var firstScrollUp = false; // Параметр начала сколла вверх
+    //     var firstScrollDown = false; // Параметр начала сколла вниз
+    //
+    //     // Если скроллим
+    //     if ( scrolled > 0 ) {
+    //         // Если текущее значение скролла > предыдущего, т.е. скроллим вниз
+    //         if ( scrolled > scrollPrev ) {
+    //             firstScrollUp = false; // Обнуляем параметр начала скролла вверх
+    //             // Если меню видно
+    //             if ( scrolled < header.height() + header.offset().top ) {
+    //                 // Если только начали скроллить вниз
+    //                 if ( firstScrollDown === false ) {
+    //                     var topPosition = header.offset().top; // Фиксируем текущую позицию меню
+    //                     header.css({
+    //                         "top": topPosition + "px"
+    //                     });
+    //                     firstScrollDown = true;
+    //                 }
+    //                 // Позиционируем меню абсолютно
+    //                 header.css({
+    //                     "position": "absolute"
+    //                 });
+    //                 // Если меню НЕ видно
+    //             } else {
+    //                 // Позиционируем меню фиксированно вне экрана
+    //                 header.css({
+    //                     "position": "fixed",
+    //                     "top": "-" + header.height() + "px"
+    //                 });
+    //             }
+    //
+    //             // Если текущее значение скролла < предыдущего, т.е. скроллим вверх
+    //         } else {
+    //             firstScrollDown = false; // Обнуляем параметр начала скролла вниз
+    //             // Если меню не видно
+    //             if ( scrolled > header.offset().top ) {
+    //                 // Если только начали скроллить вверх
+    //                 if ( firstScrollUp === false ) {
+    //                     var topPosition = header.offset().top; // Фиксируем текущую позицию меню
+    //                     header.css({
+    //                         "top": topPosition + "px"
+    //                     });
+    //                     firstScrollUp = true;
+    //                 }
+    //                 // Позиционируем меню абсолютно
+    //                 header.css({
+    //                     "position": "absolute"
+    //                 });
+    //             } else {
+    //                 // Убираем все стили
+    //                 header.removeAttr("style");
+    //             }
+    //         }
+    //         // Присваеваем текущее значение скролла предыдущему
+    //         scrollPrev = scrolled;
+    //     }
+    // });
     //--==== / Hid Menu scroll  ====--//
 
    //=====    reviews_carousel plugin ======//
@@ -370,7 +370,7 @@ $(document).ready(function(){
     var scrollTop = 0;
     var pageTop= $('.js-page-top');
     var pageBottom= $('.js-page-bottom');
-    //var ParralaxScrollStep = 2;
+    var ParralaxScrollStep = 2;
     var ParralaxTimeAnimation = 500;
     var parralaxScrollAnimation = false;
     var parralaxScrollAnimationTop = false;
